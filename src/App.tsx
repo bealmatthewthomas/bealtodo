4866de4b377daf6cfe147ab3e648df805e1a4b55
Matthew Beal <bealmatthewthomas@gmail.com>
2022-07-10 14:31:03 -0400

    working on form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import axios from 'axios'
+import { Form, Formik } from 'formik'
+import React from 'react'
 import './App.css'
-
-// import { useTodos } from './hooks/useTodos'
 import logo from './logo.svg'
 import { ToDo } from './types/schemata'
+import { styled } from 'styled-components'
 
 function App() {
+  //maintain state
+  const [todos, setTodos] = React.useState<ToDo[]>([])
+  React.useEffect(() => {
+    showAllTodos(setTodos)
+  }, [])
+
+  const todoCreateInitialValues: ToDo = {
+    title: '',
+    description: '',
+    urgency: 0,
+    importance: 0,
+    deadline: undefined,
+    status: 'Not Started',
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -13,10 +30,90 @@ function App() {
           <button onClick={() => createTodo(myTodo)}>🪂 Create Todo</button>
           <button onClick={() => helloWorld()}>🪂 Sanity Check</button>
         </div>
+        <div className="todoGrid"></div>
+        <ul>
+          {todos.map((todo, idx) => (
+            <li key={idx}>{todo.title}</li>
+          ))}
+        </ul>
+        {/* <Formik
+          initialValues={todoCreateInitialValues}
+          onSubmit={(values, actions) => {
+            console.log(values)
+            // createTodo(values)
+          }}
+        >
+          {(props) => (
+            <form onSubmit={props.handleSubmit}>
+              <input
+                type="text"
+                onChange={props.handleChange}
+                onBlur={props.handleBlur}
+                value={props.values.title}
+                name="title"
+              />
+              <input
+                type="text"
+                onChange={props.handleChange}
+                onBlur={props.handleBlur}
+                value={props.values.description}
+                name="description"
+              />
+              <input
+                type="text"
+                onChange={props.handleChange}
+                onBlur={props.handleBlur}
+                value={props.values.urgency}
+                name="name"
+              />
+              {props.errors && <div id="feedback">{props.errors}</div>}
+              <button type="submit">Submit</button>
+            </form>
+          )}
+        </Formik> */}
+        <Formik
+          initialValues={todoCreateInitialValues}
+          onSubmit={(values, actions) => {
+            console.log(values)
+            // createTodo(values)
+          }}
+        >
+          {(props) => (
+            <Form onSubmit={props.handleSubmit}>
+              <input
+                type="text"
+                onChange={props.handleChange}
+                onBlur={props.handleBlur}
+                value={props.values.title}
+                name="title"
+              />
+              <input
+                type="text"
+                onChange={props.handleChange}
+                onBlur={props.handleBlur}
+                value={props.values.description}
+                name="description"
+              />
+              <input
+                type="text"
+                onChange={props.handleChange}
+                onBlur={props.handleBlur}
+                value={props.values.urgency}
+                name="name"
+              />
+              <button type="submit">Submit</button>
+            </Form>
+          )}
+        </Formik>
       </header>
     </div>
   )
 }
+
+const TodoGrid = styled.div`
+  display: 'grid';
+  min-width: '500px';
+`
 
 function createTodo(data: ToDo) {
   return fetch('/.netlify/functions/todo-crud', {
@@ -29,6 +126,18 @@ function createTodo(data: ToDo) {
     .catch((reason) => {
       console.log(reason)
     })
+}
+
+//for now maintain state here
+function showAllTodos(setState) {
+  return axios.get('/.netlify/functions/todo-crud/').then((res) => {
+    const todos = res.data
+    const formattedTodos = todos.map((todo: any) => {
+      return todo.data
+    })
+
+    setState(formattedTodos)
+  })
 }
 
 function helloWorld() {
